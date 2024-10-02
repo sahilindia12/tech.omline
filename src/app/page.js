d80764722216b5e737c62d24847f201740b8a4d3
sahilindia12 <indiasahil44@gmail.com>
2024-10-02 08:20:52 -0700

    fsdfsdaf

--- src/app/page.js
+++ src/app/page.js
@@ -12,8 +12,10 @@ export default  function  page() {
     <div>page
       <Link href={'./nextjs/nextjs_not_found'}>next js</Link>   
 
-      <Link href={'./ch'}>ch</Link>   
-      <Link href={'./map'}>map</Link>   
+      <Link href={'./ch'}>ch</Link>    <br/>
+      <Link href={'./map'}>map</Link>   <br/>
+
+      <Link href={'/new'}>go to new page now</Link> <br/>
 
 
       <Link href={'./nextjs/nextjs_api'}>api next js</Link>   
@@ -24,6 +26,31 @@ export default  function  page() {
       <Link href={'reactjs/reactjsinputget'}>react js</Link>
       <Link href={'./javasript/jsarry'}>api</Link>   <br/>
       <Link href={'./github/git'}>How to add existing folder to GitHub repository </Link>
+
+     <div className='   bg-green-500  grid grid-cols-4 sm:grid-cols-1'>
+
+        <div className=' mx-2 bg-red-50 my-7'>
+
+          1
+        </div>
+        <div className=' mx-2 bg-red-50 my-7'>
+
+2
+</div>
+<div className=' mx-2 bg-red-50 my-7'>
+
+3
+</div>
+<div className=' mx-2 bg-red-50 my-7'>
+
+4
+</div>
+
+     </div>
+
+
+
+
     </div>
   )
 }
---
+++ src/app/reactnetive/reactnetiveerror/page.js
@@ -0,0 +1,61 @@
+
+'use client'
+
+import React from 'react';
+import { toast } from 'react-toastify';
+
+const Page = () => {
+    return (
+     <div className=' mx-5 my-5'>
+
+
+<div className=' grid grid-cols-2 sm:grid-cols-1'>
+
+<div className=' my-1 w-[75%]  md:h-[44%]  flex justify-center items-center rounded-md'>
+
+<img className=' ' src='../rnsreen.PNG'/>
+</div>
+
+
+<div>
+
+
+<h1 className=''>this is react netive errror page you can resoelw your error jsut see this error </h1>
+<p className=' leading-10'>
+
+With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:With a static export, Next.js can be deployed and hosted on any web server that can serve HTML/CSS/JS static assets.
+
+When running next build, Next.js generates the static export into the out folder. For example, let's say you have the following routes:
+
+</p>
+</div>
+
+
+<div className=' rounded-md w-[22vw]  bg-green-600 flex justify-center items-center'>
+
+<button className=' text-white text-2xl ' onClick={()=>toast.success("hello mot")}>tost now </button>
+</div>
+</div>
+
+ 
+     </div>
+    );
+}
+
+export default Page;
